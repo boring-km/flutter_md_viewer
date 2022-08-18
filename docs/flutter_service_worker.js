90574b3254060d9530ef7d47d446ec291c600066
boring-km <kms0644804@naver.com>
2022-08-18 16:52:47 +0900

    화면이 제대로 안나와서 예전 코드 써보기 2

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8d3a4d6c76b35935ad8e3bf6c41d378a",
-"index.html": "3e5e355bf893f16510f8202b8ff770ad",
-"/": "3e5e355bf893f16510f8202b8ff770ad",
+"index.html": "422f643dc872b7aa5b54dc0f3313176c",
+"/": "422f643dc872b7aa5b54dc0f3313176c",
 "main.dart.js": "ebb59c76485e2a61323d1fb323d05328",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -18,7 +19,11 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/assets/test.md": "2c4ff6ffb7d004e0c7c6314fb1a26134"
+"assets/assets/test.md": "2c4ff6ffb7d004e0c7c6314fb1a26134",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
 };
 
 // The application shell files that are downloaded before a service worker can
